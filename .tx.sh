892b235d3a08198becd95fc14265ff6fe21f4cec
Serge Zarembsky <serge@ghostery.com>
2018-05-16 09:16:52 -0400

    Update .tx.sh

--- .tx.sh
+++ .tx.sh
@@ -40,8 +40,7 @@ tx_push() {
 tx_pull() {
   # Only run once, and only for $TX_TAG tag
   echo $TRAVIS_JOB_NUMBER | grep "\.1$"
-  # if [ $? -eq 0 ] && [ $TRAVIS_TAG = $TX_TAG ]
-  if [ $? -eq 0 ] && [ $TRAVIS_BRANCH = $TX_BRANCH ]
+  if [ $? -eq 0 ] && [ $TRAVIS_TAG = $TX_TAG ]
     then
       tx_init
       tx pull --all --force
